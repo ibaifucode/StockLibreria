57a977d25f4598765ac3339abf7ddf9e17c34e36
Ibai Fuentes Palacios <133500805+ibaifucode@users.noreply.github.com>
2023-05-13 13:09:58 +0200

    connect.java
    eliminacion de comentarios

--- connect.java
+++ connect.java
@@ -28,55 +28,5 @@ public class connect {
             }
     }
 
-
-    //public List<String[]> obtenerRegistros(String nombreTabla) {
-            //List<String[]> listaRegistros = new ArrayList<>();
-            //try {
-
-                //Statement sentencia = conexion.createStatement();
-                //ResultSet resultados = sentencia.executeQuery("SELECT * FROM " + nombreTabla);
-                //ResultSetMetaData metadata = resultados.getMetaData();
-                //int numColumnas = metadata.getColumnCount();
-
-                //while (resultados.next()) {
-                    //String[] fila = new String[numColumnas];
-                    //for (int i = 1; i <= numColumnas; i++) {
-                        //fila[i - 1] = resultados.getString(i);
-                    //}
-                    //listaRegistros.add(fila);
-                //}
-
-                //resultados.close();
-                //sentencia.close();
-                //conexion.close();
-            //} catch (SQLException e) {
-                //e.printStackTrace();
-            //}
-            //return listaRegistros;
-        //}
-
-    
-
-    //public String BuscarDNI(String DNI){
-        //String passwd = null;
-        //String sentencia = "SELECT contrasena FROM trabajadores WHERE dni=?";
-        //try {
-            //PreparedStatement statement = conexion.prepareStatement(sentencia);
-            //statement.setString(1, DNI);
-            //ResultSet resultado = statement.executeQuery();
-            //if (!resultado.next()) {
-                //System.out.println("No se encontró ninguna contraseña para el DNI proporcionado.");
-            //} else {
-                //passwd = resultado.getString("contrasena");
-                //System.out.println("Contraseña encontrada:" + passwd);
-            //}
-        //} catch (SQLException e) {
-            //System.out.println("Error al buscar DNI:" + e.getMessage());
-        //}
-        //return passwd;
-    //}
-
-
-
 }
 
